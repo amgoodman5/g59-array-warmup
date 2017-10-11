b84939c7f15ef6785b3a50cc719cd0d418a4f018
Jason Dulin <jas.dulin@gmail.com>
2017-10-11 10:42:47 -0700

    double matrix left

--- filter/index.js
+++ filter/index.js
@@ -1,15 +1,22 @@
 function onlyEven(array) {
-  var a = [4,5,7,8,14,45,76];
-  return a.filter(function(val){return val%2!==0})
+  return array.filter(function(val){return val%2===0})
 }
 // refactor with filter!! OMG!
 
 function onlyOneWord(array) {
-  
+  return array.filter(function(word){
+    return (word.includes(' ') === false)
+    //if has a spacdont retrun it
+    //if word.split('').length <= 1
+  })
 }
 
 function positiveRowsOnly(array) {
-  //is Dan human?
+  return array.filter(function(row){
+    return row.every(function(pos) {
+      return pos >= 0;
+    });
+  });
 }
 
 module.exports = {
