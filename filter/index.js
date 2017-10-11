74ac07c7b0d5cf8cc98f3cb3417fbcd9a7100fd0
KevinMAnderson <kma2k4@hotmail.com>
2017-10-11 14:33:28 -0600

    all but 1 test should pass

--- filter/index.js
+++ filter/index.js
@@ -1,21 +1,41 @@
 function onlyEven(array) {
-  const evens = [];
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] % 2 === 0) {
-      evens.push(array[i]);
-    }
-  }
-  return evens;
-}
+    const evens = [];
 
+        array.filter((number)=>{
+            if (number % 2 === 0) {
+                evens.push(number);
+              }
+        });
+      return evens;
+}
 // refactor with filter!! OMG!
 
 function onlyOneWord(array) {
   //where are the snacks?
+  let newArray = array.filter(snack=>{
+
+     if(!snack.includes(' ')) {
+         return snack
+     }
+
+  });
+  return newArray;
 }
 
 function positiveRowsOnly(array) {
   //is Dan human?
+  let newArray = array.filter((row)=>{
+
+      row.filter(int=>{
+
+          if (int % 2 !== 0) {
+              array.splice(row,1);
+          }
+
+      });
+
+  });
+  return newArray
 }
 
 module.exports = {
--- map/index.js
+++ map/index.js
@@ -1,25 +1,21 @@
 function multiplyBy10(array) {
-  for (let i = 0; i < array.length; i++) {
-    array[i] = array[i] * 10;
-  }
-  return array;
+
+     let newArray = array.map((number)=>{
+        return number * 10;
+    });
+    return newArray;
 }
 
 function onlyVowels(array) {
-  for (var i = 0; i < array.length; i++) {
-    array[i] = array[i].replace(/[^aeiou]*/gi, '');
-  }
-  return array;
-}
+    let newArray = array.map(vowel=>{
+        return vowel.replace(/[^aeiou]*/gi, '');
+    });
 
+  return newArray;
+}
 function doubleMatrix(array) {
-  for (var i = 0; i < array.length; i++) {
-    for (var x = 0; x < array[i].length; x++) {
-      array[i][x] = array[i][x] * 2;
-    }
-  }
-  return array;
-  // return array.map(nums => nums.map(n => n * 2)); // your code here
+
+  return array.map(nums => nums.map(n => n * 2)); // your code here
 }
 
 module.exports = {
