ccd347e6aab64231ff271e2b477dc3f6deb5505b
Maire Chew <mairechew@gmail.com>
2017-10-11 13:54:09 -0600

    refactor functions

--- filter/index.js
+++ filter/index.js
@@ -1,19 +1,31 @@
-function onlyEven(array) {
-  const evens = [];
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] % 2 === 0) {
-      evens.push(array[i]);
-    }
-  }
-  return evens;
-}
+// function onlyEven(array) {
+//   const evens = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 === 0) {
+//       evens.push(array[i]);
+//     }
+//   }
+//   return evens;
+// }
+
 // refactor with filter!! OMG!
+function onlyEven(array) {
+  var evens = array.filter(function(even){
+    return (even % 2 === 0)
+  })
+}
 
 function onlyOneWord(array) {
+  var single = array.filter(function(word) => {
+    return (word.length === 1)
+  })
   //where are the snacks?
 }
 
 function positiveRowsOnly(array) {
+  var posRow = array.filter(function(nums) => {
+    return (.indexOf(nums) % 2 === 0)
+  })
   //is Dan human?
 }
 
--- map/index.js
+++ map/index.js
@@ -1,25 +1,21 @@
 function multiplyBy10(array) {
-  for (let i = 0; i < array.length; i++) {
-    array[i] = array[i] * 10;
-  }
-  return array;
+  return array.map((num) => {
+    num = num * 10;
+  })
 }
 
 function onlyVowels(array) {
-  for (var i = 0; i < array.length; i++) {
-    array[i] = array[i].replace(/[^aeiou]*/gi, '');
-  }
-  return array;
+  return array.map((x) => {
+    x = x.replace(/[^aeiou]*/gi, '')
+  })
 }
 
 function doubleMatrix(array) {
-  for (var i = 0; i < array.length; i++) {
-    for (var x = 0; x < array[i].length; x++) {
-      array[i][x] = array[i][x] * 2;
-    }
-  }
-  return array;
-  // return array.map(nums => nums.map(n => n * 2)); // your code here
+  return array.map((x) => {
+    x.map((i) => {
+      i = i * 2;
+    })
+  })
 }
 
 module.exports = {
